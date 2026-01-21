cb902acb90e45b4928fccf56b628046d5343efa5
ryuudachi <juanignaciolp2001@gmail.com>
2026-01-21 18:31:57 +0100

    commit 0.2.3

--- statics/js/gameData.js
+++ statics/js/gameData.js
@@ -3,7 +3,7 @@ const gamesData = [
     {
         id: "fatal-frame-2",
         title: "FATAL FRAME II: Crimson Butterfly",
-        description: "El remake de FATAL FRAME II: Crimson Butterfly ha encendido a toda la comunidad de NebulaPlay...",
+        description: "El remake de FATAL FRAME II: Crimson Butterfly ha encendido a toda la comunidad de NebulaPlay. La legendaria historia de las hermanas Mio y Mayu regresa con gráficos totalmente renovados, efectos de sonido envolventes y una atmósfera tan perturbadora que muchos jugadores admiten no poder jugar más de 20 minutos seguidos. El nuevo modo “Lente Carmesí” está siendo lo más comentado: permite capturar espíritus con mecánicas más precisas, filtros visuales reactivos y eventos aleatorios que cambian cada partida. La aldea perdida de Minakami nunca había sido tan inquietante.",
         mainVideo: "https://drive.google.com/file/d/1WxEtAgg5uVRkkgi1zRqx-0oy41OAxlV4/preview",
         thumbnails: [
             "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/257224970/b2e4156e02f3fda59fa65b5ddeaaf9bddb2730b4/movie_full.jpg?t=1762906663",
@@ -22,58 +22,58 @@ const gamesData = [
     {
         id: "silent-2",
         title: "Silent Hill II",
-        description: "El remake de Silent Hill II ha encendido a toda la comunidad de NebulaPlay...",
-        mainVideo: "https://drive.google.com/file/d/1WxEtAgg5uVRkkgi1zRqx-0oy41OAxlV4/preview",
+        description: "Al investigar una carta de su difunta esposa, James regresa al lugar donde compartieron tantos recuerdos: Silent Hill. Lo que encuentra es un pueblo fantasma, acechado por monstruos inquietantes y envuelto en una densa niebla. Enfréntate a las criaturas, resuelve puzles y busca pistas sobre tu esposa en este remake de SILENT HILL 2.",
+        mainVideo: "https://drive.google.com/file/d/1jjjA1A2a1TElYXol2HhxviM1xb77cSLn/preview",
         thumbnails: [
-            "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/257224970/b2e4156e02f3fda59fa65b5ddeaaf9bddb2730b4/movie_full.jpg?t=1762906663",
-            "/img/fatal1.jpg",
-            "/img/fatal2.jpg",
-            "/img/fatal3.jpg",
-            "/img/fatal4.jpg",
-            "/img/fatal5.jpg",
-            "/img/fatal6.jpg"
+            "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/257063160/b8d8d574fe93e6d924047ec3bcc7414115d6c096/movie_full.jpg?t=1744248681",
+            "/img/silent1.jpg",
+            "/img/silent2.jpg",
+            "/img/silent3.jpg",
+            "/img/silent4.jpg",
+            "/img/silent5.jpg",
+            "/img/silent6.jpg"
         ],
         thumbnailVideos: [
-            "https://drive.google.com/file/d/1WxEtAgg5uVRkkgi1zRqx-0oy41OAxlV4/preview"
+            "https://drive.google.com/file/d/1jjjA1A2a1TElYXol2HhxviM1xb77cSLn/preview"
         ],
-        posterImage: "/img/Frame 31.png"
+        posterImage: "/img/silentposter.jpg"
     },
     {
         id: "honkai",
-        title: "Honkai Star Rail",
-        description: "El remake de Silent Hill II ha encendido a toda la comunidad de NebulaPlay...",
-        mainVideo: "https://drive.google.com/file/d/1WxEtAgg5uVRkkgi1zRqx-0oy41OAxlV4/preview",
+        title: "Honkai: Star Rail",
+        description: "Honkai: Star Rail es el RPG de fantasía espacial de HoYoverse. Sube al Expreso Astral y descubre las maravillas infinitas de la galaxia en este viaje lleno de emoción y aventuras.",
+        mainVideo: "https://drive.google.com/file/d/1_bqKuWiKQB52IyLgJpGsg6CsJapZ-mCc/preview",
         thumbnails: [
-            "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/257224970/b2e4156e02f3fda59fa65b5ddeaaf9bddb2730b4/movie_full.jpg?t=1762906663",
-            "/img/fatal1.jpg",
-            "/img/fatal2.jpg",
-            "/img/fatal3.jpg",
-            "/img/fatal4.jpg",
-            "/img/fatal5.jpg",
-            "/img/fatal6.jpg"
+            "https://cdn1.epicgames.com/spt-assets/6f3979ff608f42e286c83507a69b27f5/honkai-star-rail-video-1ef7w.jpg",
+            "/img/honkai1.png",
+            "/img/honkai2.png",
+            "/img/honkai3.png",
+            "/img/honkai4.png",
+            "/img/honkai5.png",
+            "/img/honkai6.jpg"
         ],
         thumbnailVideos: [
-            "https://drive.google.com/file/d/1WxEtAgg5uVRkkgi1zRqx-0oy41OAxlV4/preview"
+            "https://drive.google.com/file/d/1_bqKuWiKQB52IyLgJpGsg6CsJapZ-mCc/preview"
         ],
-        posterImage: "/img/Frame 31.png"
+        posterImage: "/img/honkaiposter.jpg"
     },
     {
         id: "fortnite",
         title: "Fortnite",
-        description: "El remake de Silent Hill II ha encendido a toda la comunidad de NebulaPlay...",
-        mainVideo: "https://drive.google.com/file/d/1WxEtAgg5uVRkkgi1zRqx-0oy41OAxlV4/preview",
+        description: "Lánzate a la acción de siempre o descubre algo nuevo con tu escuadrón. Consigue ser el último jugador en pie en Battle Royale. Siente la nostalgia en Fortnite: Orígenes. Juega de forma táctica en Recarga. Conviértete en la estrella de Fortnite Festival. Explora los mundos de LEGO Fortnite. Siempre hay algo nuevo que hacer para ti y tus amigos.",
+        mainVideo: "https://drive.google.com/file/d/1mQZp4Z7FpPJXia6fSaMfESj0uPiVOfBm/preview",
         thumbnails: [
-            "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/257224970/b2e4156e02f3fda59fa65b5ddeaaf9bddb2730b4/movie_full.jpg?t=1762906663",
-            "/img/fatal1.jpg",
-            "/img/fatal2.jpg",
-            "/img/fatal3.jpg",
-            "/img/fatal4.jpg",
-            "/img/fatal5.jpg",
-            "/img/fatal6.jpg"
+            "https://cdn2.unrealengine.com/fnbr-39-20-safflower-msee-sonylauncher-coverart-3840x2160-logo-3840x2160-b7ab271c924b.jpg",
+            "/img/fortnite1.jpg",
+            "/img/fortnite2.jpg",
+            "/img/fortnite3.jpg",
+            "/img/fortnite4.jpg",
+            "/img/fortnite5.jpg",
+            "/img/fortnite6.jpg"
         ],
         thumbnailVideos: [
-            "https://drive.google.com/file/d/1WxEtAgg5uVRkkgi1zRqx-0oy41OAxlV4/preview"
+            "https://drive.google.com/file/d/1mQZp4Z7FpPJXia6fSaMfESj0uPiVOfBm/preview"
         ],
-        posterImage: "/img/Frame 31.png"
+        posterImage: "/img/fortniteposter.jpg"
     }
 ];
